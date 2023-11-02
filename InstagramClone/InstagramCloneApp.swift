3b31bb81b10547a4b4c0c2f644df9d8b706e38ed
Vefa Kosova <109106432+Argentha@users.noreply.github.com>
2023-11-02 15:09:39 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Vefa Kosova on 2.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
